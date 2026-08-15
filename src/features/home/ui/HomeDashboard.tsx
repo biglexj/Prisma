d2aa6196df70d62de82a9395c153434e932b16cf
Biglex J <biglex.rv@gmail.com>
2026-08-14 22:39:58 -0500

    checkpoint: session 2026-08-14 - ajuste a auto-fit para ocupar el 100% del ancho del inicio sin huecos laterales

--- src/features/home/ui/HomeDashboard.tsx
+++ src/features/home/ui/HomeDashboard.tsx
@@ -6,7 +6,7 @@ import { VideoThumbnail } from "../../visual_library/ui/VideoThumbnail";
 import { Icon } from "../../../shared/ui/Icon";
 import "./home-dashboard.css";
 
-const HOME_ROW_ITEMS_LIMIT = 10;
+const HOME_ROW_ITEMS_LIMIT = 12;
 
 interface HomeDashboardProps {
   musicFolders: MusicFolderSource[];
